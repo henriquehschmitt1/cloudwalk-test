71daba66eba6eb76115dadd7bcb3a179171bbf16
henriquehschmitt1 <henriquehschmitt@hotmail.com>
2024-06-18 19:42:26 -0300

    feat: setting project up

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import indexRouter from "./routes/index.js";
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+    // this.swagger();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use("/", indexRouter);
+  }
+
+  // swagger() {
+  // 	this.server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  // }
+}
+
+export default new App().server;
