f57066c62afe70cabe7ed37df3169fbeb2d8d2f0
henriquehschmitt1 <henriquehschmitt@hotmail.com>
2024-06-21 16:14:53 -0300

    refactor: removing unused methods

--- src/entity/QuakeGameEntity.js
+++ src/entity/QuakeGameEntity.js
@@ -23,20 +23,7 @@ class QuakeGame {
     this.killsByMeans[weapon] += 1;
   }
 
-  removePlayer(playerName) {
-    if (this.players.includes(playerName)) {
-      this.players.delete(playerName);
-      delete this.kills[playerName];
-    }
-  }
-
   increaseKillByOne(playerName) {
-    if (!this.kills.hasOwnProperty(playerName)) {
-      throw {
-        status: 404,
-        message: "Player not found",
-      };
-    }
     this.kills[playerName] += 1;
     this.totalKills += 1;
   }
@@ -46,16 +33,6 @@ class QuakeGame {
       this.kills[playerName] === 0 ? null : (this.kills[playerName] -= 1);
     }
     this.totalKills += 1;
-  }
-
-  resetGame() {
-    this.totalKills = 0;
-    this.players = new Map();
-    this.kills = {};
-  }
-
-  getPlayersAsArray() {
-    return Array.from(this.players.keys());
   }
 }
 
